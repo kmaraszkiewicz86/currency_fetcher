e81d78f876abdaae4d91330c689345ce18d99206
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-05-04 14:25:47 +0200

    Basic structure of project

---
+++ webAPI/CurrencyFetcherApi/CurrencyFetcherApi.Tests/Controllers/CurrencyControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CurrencyFetcherApi.Tests.Controllers
+{
+    public class CurrencyControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ webAPI/CurrencyFetcherApi/CurrencyFetcherApi/Controllers/CurrencyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyFetcherApi.Controllers
+{
+    [ApiController]
+    [Route("api/Currency")]
+    public class CurrencyController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "USD", "PLN", "EUR"
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Summaries);
+        }
+    }
+}
